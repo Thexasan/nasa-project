a95ea183149433da7fc1fbd6bd0201929d5c5429
AED11 <akhmetoverajdev@gmail.com>
2024-10-06 20:33:40 +0500

    dubai

--- src/components/pages/home/section6/Section6.jsx
+++ src/components/pages/home/section6/Section6.jsx
@@ -76,67 +76,87 @@ const Section6 = () => {
     {
       title: "Ecological Sustainability",
       description: "Reducing carbon emissions and using renewable energy.",
-      img: "src/components/pages/home/section6/img/sustainability-compressed.webp"
+      img: "src/components/pages/home/section6/img/sustainability-compressed.webp",
     },
     {
       title: "Reforestation",
       description: "Planting trees and restoring ecosystems.",
-      img: 'src/components/pages/home/section6/img/Reforestation-–-Is-It-Actually-Important.webp'
+      img: "src/components/pages/home/section6/img/Reforestation-–-Is-It-Actually-Important.webp",
     },
     {
       title: "Circular Economy",
       description: "Recycling and optimizing resource use.",
-      img: "src/components/pages/home/section6/img/shutterstock_2109112064-scaled-e1663221705810.jpg"
+      img: "src/components/pages/home/section6/img/shutterstock_2109112064-scaled-e1663221705810.jpg",
     },
     {
       title: "Water Conservation",
       description: "Saving water and using it efficiently.",
-      img: "src/components/pages/home/section6/img/water-conservation-header-1.jpg"
+      img: "src/components/pages/home/section6/img/water-conservation-header-1.jpg",
     },
     {
       title: "Green Cities",
       description: "Building energy-efficient cities with green spaces.",
-      img: "src/components/pages/home/section6/img/Green-City-scaled.jpg"
+      img: "src/components/pages/home/section6/img/Green-City-scaled.jpg",
     },
     {
       title: "Environmental Education",
       description: "Teaching eco-friendly habits and sustainability.",
-      img: "src/components/pages/home/section6/img/environmental-education.jpg"
+      img: "src/components/pages/home/section6/img/environmental-education.jpg",
     },
     {
       title: "Green Technologies",
       description: "Using clean energy to reduce pollution.",
-      img: "src/components/pages/home/section6/img/199_1676390451.png"
+      img: "src/components/pages/home/section6/img/199_1676390451.png",
     },
     {
       title: "Responsible Consumption",
       description: "Choosing sustainable products and reducing waste.",
-      img: 'src/components/pages/home/section6/img/images.jpg'
+      img: "src/components/pages/home/section6/img/images.jpg",
     },
   ];
 
   return (
     <div className="bg-[black]">
-      <h1 className="text-center p-[50px] text-[50px] text-[white] ">How to improve our planet</h1>
+      <h1 className="text-center p-[50px] text-[50px] text-[white] ">
+        How to improve our planet
+      </h1>
       <div className="flex flex-wrap justify-center max-w-[1400px] m-auto">
-        {data.map((el, index) => (
-          <div
-            className="card"
-            key={index}
-            ref={(el) => (cardRefs.current[index] = el)}
-          >
-            <div className="card__content">
-              <div className="card__gloss"></div>
-              <img
-                className="card__image"
-                src={el.img}
-                alt=""
-              />
-              <h2 className="card__title">{el.title}</h2>
-              <p className="card__description">{el.description}</p>
-            </div>
-          </div>
-        ))}
+        {data.map((el, i) => {
+          if(i < 4){
+            return (
+              <div
+              data-aos="fade-up"
+                className="card"
+                key={index}
+                ref={(el) => (cardRefs.current[index] = el)}
+              >
+                <div className="card__content">
+                  <div className="card__gloss"></div>
+                  <img className="card__image" src={el.img} alt="" />
+                  <h2 className="card__title">{el.title}</h2>
+                  <p className="card__description">{el.description}</p>
+                </div>
+              </div>
+            );
+          }else{
+            return (
+              <div
+    data-aos="fade-down"
+                className="card"
+                key={index}
+                ref={(el) => (cardRefs.current[index] = el)}
+              >
+                <div className="card__content">
+                  <div className="card__gloss"></div>
+                  <img className="card__image" src={el.img} alt="" />
+                  <h2 className="card__title">{el.title}</h2>
+                  <p className="card__description">{el.description}</p>
+                </div>
+              </div>
+            );
+          }
+          
+        })}
       </div>
     </div>
   );
