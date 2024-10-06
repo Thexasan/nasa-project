aa8bf9f582fe0a62cf34efd6eb504943c0f1efc5
AED11 <akhmetoverajdev@gmail.com>
2024-10-06 14:52:27 +0500

    axios

--- src/components/pages/home/section1/Section1.jsx
+++ src/components/pages/home/section1/Section1.jsx
@@ -1,6 +1,14 @@
+import axios from 'axios'
 import React from 'react'
 
 const Section1 = () => {
+  async function  getMap() {
+    try{
+      let {data} = axios.get("")
+    }catch(error){
+      console.log(error)
+    }
+  }
   return (
     <div>Section1</div>
   )
