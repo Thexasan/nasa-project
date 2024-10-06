78ca777b1e839bb7eafbbdbba149caa84fc1ba38
Hasaan <husenovhasan5577@gmail.com>
2024-10-06 14:31:34 +0500

    routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold text-center underline">
-      Hello world!
-    </h1>
+    <>
+    
+    </>
   )
 }
 
 export default App
+
+
