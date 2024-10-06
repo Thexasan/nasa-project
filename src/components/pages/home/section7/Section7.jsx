093348afb2412386c3ea2cef1ed4b97e53cf9d4b
AED11 <akhmetoverajdev@gmail.com>
2024-10-06 20:27:12 +0500

    remba

--- src/components/pages/home/section7/Section7.jsx
+++ src/components/pages/home/section7/Section7.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Section7 = () => {
   return (
-    <div className='bg-[black]'>
+    <div className='bg-[black] flex flex-wrap'>
       
 
-      <h1 className='text-[white] text-[40px] w-[50vw] p-[40px]'>Earth is not just a place where we live; it’s a living organism that gives us endless possibilities. Every step toward protecting it is a step toward creating a world where our descendants will breathe clean air, drink pure water, and see the sky as the first human once did. Your care today is a priceless gift for all who will come tomorrow.</h1>
+      <h1 className='text-[white] text-[20px] xl:text-[40px] w-[70vw] xl:w-[70vw] p-[40px]'>Earth is not just a place where we live; it’s a living organism that gives us endless possibilities. Every step toward protecting it is a step toward creating a world where our descendants will breathe clean air, drink pure water, and see the sky as the first human once did. Your care today is a priceless gift for all who will come tomorrow.</h1>
+      <img className='sm:w-[60vw] md:w-[60vw] lg:w-[60vw] xl:w-[50vw] w-[60vw]' src="src/components/pages/home/section6/img/pngwing.com (10).png" alt="" />
     </div>
   )
 }
