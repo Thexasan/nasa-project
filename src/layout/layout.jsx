3d149486cf3102890405f518644dd60f56cb9ae6
Amsurur <abdulloevmuhammadsurur@gmail.com>
2024-10-06 17:15:30 +0500

    add

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/layout/header/Header";
 import Footer from "../components/layout/footer/Footer";
 
-export default function Layout(){
-  return <>
-    <Header />
-    <Outlet />
-    <Footer />
-  </>
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,15 +6,17 @@ import Section5 from "../../components/pages/home/section5/Section5";
 import Section6 from "../../components/pages/home/section6/Section6";
 import Section7 from "../../components/pages/home/section7/Section7";
 
-export default function Home(){
-  return <>
-  Home
-  <Section1 />
-  <Section2/>
-  <Section3 />
-  <Section4 />
-  <Section5 />
-  <Section6 />
-  <Section7 />
-  </>
+export default function Home() {
+  return (
+    <>
+      Home
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+      <Section7 />
+    </>
+  );
 }
