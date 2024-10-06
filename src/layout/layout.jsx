049b458a4c4bf31bf7cc7ce343d8f3374f2e7298
AED11 <akhmetoverajdev@gmail.com>
2024-10-06 20:29:46 +0500

    nasa

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/layout/header/Header";
 import Footer from "../components/layout/footer/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Layout(){
+  useEffect(() => { 
+    AOS.init(); 
+    AOS.refresh(); 
+  }, []);
   return <>
     <Header />
     <Outlet />
